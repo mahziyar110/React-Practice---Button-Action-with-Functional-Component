d5edb9f127112f339da5a5a813004be1635123a8
mahziyar110 <mahziyar110@gmail.com>
2021-08-11 01:49:21 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [show, newShow] = useState(true);
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={() => {
+        newShow(false);
+      }}>Render Paragraph</button>
+      {show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
