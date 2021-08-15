017e0ddf787ae087dc925653641b0093c24cfd1a
mahziyar110 <mahziyar110@gmail.com>
2021-08-15 20:00:08 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   const [show, newShow] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* Do not alter the main div */}
       <button id="click" onClick={() => {
         newShow(true);
       }}>Render Paragraph</button>
